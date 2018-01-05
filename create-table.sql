ca8685fc213751ec7d24539c18bc66ffd7dda2f3
AndriiU <au230677@gmail.com>
2018-01-05 22:29:55 +0200

    Create create-table.sql

---
+++ create-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE  `webee_dolina`.`comments` (
+`id` INT( 3 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`author` VARCHAR( 30 ) NOT NULL ,
+`message` TEXT NOT NULL ,
+`email` TEXT NOT NULL ,
+`date` VARCHAR( 25 ) NOT NULL
+) 
